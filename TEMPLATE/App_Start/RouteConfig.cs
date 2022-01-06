db014d662ad9b4255c2150016aa0362b7fb721d2
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-06 18:05:29 +0200

    configuration de route par default

--- TEMPLATE/App_Start/RouteConfig.cs
+++ TEMPLATE/App_Start/RouteConfig.cs
@@ -9,15 +9,15 @@ namespace TEMPLATE
 {
     public class RouteConfig
     {
-        //public static void RegisterRoutes(RouteCollection routes)
-        //{
-        //    routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-        //    routes.MapRoute(
-        //        name: "Default",
-        //        url: "{controller}/{action}/{id}",
-        //        defaults: new { controller = "Dashboard", action = "Login", id = UrlParameter.Optional }
-        //    );
-        //}
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Dashboard", action = "Login", id = UrlParameter.Optional }
+            );
+        }
     }
 }
