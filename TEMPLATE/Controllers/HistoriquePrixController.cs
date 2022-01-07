af0caf98a582ea4bd3896c3e0a0e7d93247cd101
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-07 11:18:36 +0200

    craetion de la fonction create

--- TEMPLATE/Controllers/HistoriquePrixController.cs
+++ TEMPLATE/Controllers/HistoriquePrixController.cs
@@ -27,15 +27,13 @@ namespace TEMPLATE.Controllers
             }
             return View(historique_prix);
         }
-
-        //
-        // GET: /HistoriquePrix/Create
-
         public ActionResult Create()
         {
             ViewBag.ID_qualite = new SelectList(db.qualites, "ID_qualite", "NOM_qualite");
             return View();
         }
+
+
 
         //
         // POST: /HistoriquePrix/Create
