39a5cf207160b0554c792a6c3d96cb403a0a61a1
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-07 17:03:10 +0200

    ajout une fonction index

--- TEMPLATE/Controllers/QualiteStationController.cs
+++ TEMPLATE/Controllers/QualiteStationController.cs
@@ -1,120 +1,117 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Data.Entity;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using TEMPLATE.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEMPLATE.Models;
 
-//namespace TEMPLATE.Controllers
-//{
-//    public class QualiteStationController : Controller
-//    {
-//        private RecolteEntities db = new RecolteEntities();
+namespace TEMPLATE.Controllers
+{
+    public class QualiteStationController : Controller
+    {
+        private RecolteEntities db = new RecolteEntities();
 
-//        //
-//        // GET: /QualiteStation/
+        public ActionResult Index()
+        {
+            return View(db.qualites.ToList());
+        }
 
-//        public ActionResult Index()
-//        {
-//            return View(db.qualites.ToList());
-//        }
+        //
+        // GET: /QualiteStation/Details/5
 
-//        //
-//        // GET: /QualiteStation/Details/5
+        public ActionResult Details(int id = 0)
+        {
+            qualite qualite = db.qualites.Find(id);
+            if (qualite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(qualite);
+        }
 
-//        public ActionResult Details(int id = 0)
-//        {
-//            qualite qualite = db.qualites.Find(id);
-//            if (qualite == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(qualite);
-//        }
+        //
+        // GET: /QualiteStation/Create
 
-//        //
-//        // GET: /QualiteStation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
+        //
+        // POST: /QualiteStation/Create
 
-//        //
-//        // POST: /QualiteStation/Create
+        [HttpPost]
+        public ActionResult Create(qualite qualite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.qualites.Add(qualite);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-//        [HttpPost]
-//        public ActionResult Create(qualite qualite)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                db.qualites.Add(qualite);
-//                db.SaveChanges();
-//                return RedirectToAction("Index");
-//            }
+            return View(qualite);
+        }
 
-//            return View(qualite);
-//        }
+        //
+        // GET: /QualiteStation/Edit/5
 
-//        //
-//        // GET: /QualiteStation/Edit/5
+        public ActionResult Edit(int id = 0)
+        {
+            qualite qualite = db.qualites.Find(id);
+            if (qualite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(qualite);
+        }
 
-//        public ActionResult Edit(int id = 0)
-//        {
-//            qualite qualite = db.qualites.Find(id);
-//            if (qualite == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(qualite);
-//        }
+        //
+        // POST: /QualiteStation/Edit/5
 
-//        //
-//        // POST: /QualiteStation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(qualite qualite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(qualite).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(qualite);
+        }
 
-//        [HttpPost]
-//        public ActionResult Edit(qualite qualite)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                db.Entry(qualite).State = EntityState.Modified;
-//                db.SaveChanges();
-//                return RedirectToAction("Index");
-//            }
-//            return View(qualite);
-//        }
+        //
+        // GET: /QualiteStation/Delete/5
 
-//        //
-//        // GET: /QualiteStation/Delete/5
+        public ActionResult Delete(int id = 0)
+        {
+            qualite qualite = db.qualites.Find(id);
+            if (qualite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(qualite);
+        }
 
-//        public ActionResult Delete(int id = 0)
-//        {
-//            qualite qualite = db.qualites.Find(id);
-//            if (qualite == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(qualite);
-//        }
+        //
+        // POST: /QualiteStation/Delete/5
 
-//        //
-//        // POST: /QualiteStation/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            qualite qualite = db.qualites.Find(id);
+            db.qualites.Remove(qualite);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-//        [HttpPost, ActionName("Delete")]
-//        public ActionResult DeleteConfirmed(int id)
-//        {
-//            qualite qualite = db.qualites.Find(id);
-//            db.qualites.Remove(qualite);
-//            db.SaveChanges();
-//            return RedirectToAction("Index");
-//        }
-
-//        protected override void Dispose(bool disposing)
-//        {
-//            db.Dispose();
-//            base.Dispose(disposing);
-//        }
-//    }
-//}
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
