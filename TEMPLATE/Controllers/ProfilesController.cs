000b8f127e8dbc75767dd7c963fd381b0ea04eeb
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-07 13:58:50 +0200

    ajout index

--- TEMPLATE/Controllers/ProfilesController.cs
+++ TEMPLATE/Controllers/ProfilesController.cs
@@ -12,10 +12,6 @@ namespace TEMPLATE.Controllers
     public class ProfilesController : Controller
     {
         private RecolteEntities db = new RecolteEntities();
-
-        //
-        // GET: /Profiles/
-
         public ActionResult Index()
         {
             return View(db.profiles.ToList());
