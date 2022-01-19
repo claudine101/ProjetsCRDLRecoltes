96bd662ed6c03445717d69cc33bcd8c0df54fabf
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-19 18:29:22 +0200

    ajouter un fonction delete

--- TEMPLATE/Controllers/HistoriqueStationController.cs
+++ TEMPLATE/Controllers/HistoriqueStationController.cs
@@ -14,12 +14,74 @@ namespace TEMPLATE.Controllers
         private RecolteEntities db = new RecolteEntities();
 
         public ActionResult Index()
-        {
-            var historique_station = db.historique_station.Include(h => h.station_lavage);
-            return View(historique_station.ToList());
+        {var donne = from a in db.station_lavage
+                    
+                        join z in db.zones
+                        on a.ID_Zone equals z.ID_zone
+                        join co in db.communes
+                        on z.ID_commune equals co.ID_commune
+                        join p in db.provinces
+                        on co.ID_province equals p.ID_province
+                        join h in db.historique_station 
+                        on a.ID_station equals h.ID_station
+                        select new recoltModel
+                        {
+                            ID = h.ID_histoStation,
+                            NOM_station = a.NOM_station,
+                            tel = a.TEL_station,
+                            Date_insertion=(a.DATE_insertion).Value,
+                            date = (h.DATE_desactive).Value,
+                            zone = z.NOM_zone,
+                            commune = co.NOM_commune,
+                            province = p.NOM_province
+                        };
+            ViewData["station"] = donne.ToList();
+            ViewBag.ID_provinceInactifse = new SelectList(db.provinces, "ID_province", "NOM_province");
+            return View();
         }
+        [HttpPost]
+        public ActionResult Index(int? ID_provinceState)
+        {
+            var province = (from p in db.provinces
+                            where p.ID_province == ID_provinceState
+                            select new
+                            {
 
-
+                                province = p.NOM_province
+                            }).ToList();
+            var nomprovinc = "";
+            foreach (var vp in province)
+            {
+                nomprovinc = vp.province;
+            }
+            ViewBag.nomprovince = "qui se trouve dans le province " + nomprovinc;
+            var donne = from a in db.station_lavage
+                    
+                        join z in db.zones
+                        on a.ID_Zone equals z.ID_zone
+                        join co in db.communes
+                        on z.ID_commune equals co.ID_commune
+                        join p in db.provinces
+                        on co.ID_province equals p.ID_province
+                        join h in db.historique_station 
+                        on a.ID_station equals h.ID_station
+                        where p.ID_province == ID_provinceState
+                        select new recoltModel
+                        {
+                            ID = h.ID_histoStation,
+                            NOM_station = a.NOM_station,
+                            tel = a.TEL_station,
+                            Date_insertion=(a.DATE_insertion).Value,
+                            date = (h.DATE_desactive).Value,
+                            zone = z.NOM_zone,
+                            commune = co.NOM_commune,
+                            province = p.NOM_province
+                        };
+            ViewData["station"] = donne.ToList();
+            ViewBag.ID_provinceInactifse = new SelectList(db.provinces, "ID_province", "NOM_province");
+            return View();
+        }
+        
         public ActionResult Details(int id = 0)
         {
             historique_station historique_station = db.historique_station.Find(id);
