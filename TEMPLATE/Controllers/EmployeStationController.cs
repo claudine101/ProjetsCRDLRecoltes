351321091cbce89bb8b4863b9c0014c01085a3eb
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-19 18:33:52 +0200

    modification de la fonction index

--- TEMPLATE/Controllers/EmployeStationController.cs
+++ TEMPLATE/Controllers/EmployeStationController.cs
@@ -18,10 +18,74 @@ namespace TEMPLATE.Controllers
 
         public ActionResult Index()
         {
-            var employe_station_lavage = db.employe_station_lavage.Include(e => e.station_lavage);
-            return View(employe_station_lavage.ToList());
-        }
+            var employe_statione = from e in db.employe_station_lavage
+                                       join a in db.station_lavage
+                                       on e.ID_station equals a.ID_station
 
+                                       select new recoltModel
+                                       {
+                                           ID = e.ID_employ,
+                                           NOM_station = a.NOM_station,
+                                           NOM_employe = e.NOM_employe,
+                                           PRENOM_employe = e.PRENOM_employe,
+                                           Tel_employe = e.TEL_employe,
+                                           EMAIL_employe = e.EMAIL_employe,
+                                           CNI = e.CNI,
+                                           statut = e.Statut
+
+                                       };
+            var historique = from a in db.historique_utilisateur
+                             join u in db.utilisateurs
+                             on a.ID_utilisateur equals u.ID_utilisateur
+                             select new recoltModel
+                             {
+                                 ID = a.ID_histoUtilisateur,
+                                 id = u.ID_employ.Value,
+
+                             };
+            ViewBag.station = new SelectList(db.station_lavage, "ID_station", "NOM_station");
+           
+       
+            return View(employe_statione.ToList());
+           
+        }
+        [HttpPost]
+        public ActionResult Index(int?station)
+        {
+            var province = (from p in db.station_lavage
+                            where p.ID_station == station
+                            select new
+                            {
+
+                                station = p.NOM_station
+                            }).ToList();
+            var nomprovinc = "";
+            foreach (var vp in province)
+            {
+                nomprovinc = vp.station;
+            }
+            ViewBag.nomprovince = "qui se trouve dans une station de lavage  " + nomprovinc;
+            
+            var employe_statione = from e in db.employe_station_lavage
+                                   join a in db.station_lavage
+                                   on e.ID_station equals a.ID_station
+                                   where a.ID_station == station
+                                   select new recoltModel
+                                   {
+                                       ID = e.ID_employ,
+                                       NOM_station = a.NOM_station,
+                                       NOM_employe = e.NOM_employe,
+                                       PRENOM_employe = e.PRENOM_employe,
+                                       Tel_employe = e.TEL_employe,
+                                       EMAIL_employe = e.EMAIL_employe,
+                                       CNI = e.CNI,
+                                       statut = e.Statut
+
+                                   };
+            ViewBag.station = new SelectList(db.station_lavage, "ID_station", "NOM_station");
+
+            return View(employe_statione.ToList());
+        }
         public ActionResult Details(int id = 0)
         {
             employe_station_lavage employe_station_lavage = db.employe_station_lavage.Find(id);
