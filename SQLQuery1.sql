ac93274461d0c509057b5368adf35d5143fe8f5a
claudine100 <ndayisabaclaudine01@gmail.com>
2022-02-02 11:40:11 +0200

    ADD title

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿select * from client c join recolte r
+on c.ID_client=r.ID_client
+join station_lavage s
+on r.ID_station=s.ID_station
+join employe_station_lavage e 
+on e.ID_station=s.ID_station
+where e.ID_employ=2
