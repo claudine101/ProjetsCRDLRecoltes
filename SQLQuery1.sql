96fb0c3c4855fdc072e6d25dd230459ac6e5414a
claudine100 <ndayisabaclaudine01@gmail.com>
2022-02-02 11:39:07 +0200

    ADD title

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-﻿select * from client c join recolte r
-on c.ID_client=r.ID_client
-join station_lavage s
-on r.ID_station=s.ID_station
-join employe_station_lavage e 
-on e.ID_station=s.ID_station
-where e.ID_employ=2
